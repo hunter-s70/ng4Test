3acad6968444e37c2300c66cc546ca5939ed746a
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-02-09 22:14:43 +0300

    add settings page markup

--- app/components/SettingsCmp/app.settings.ts
+++ app/components/SettingsCmp/app.settings.ts
@@ -5,6 +5,42 @@ import { Component} from '@angular/core';
 
 @Component({
     selector: 'app-settings',
-    template: `<h3>Settings</h3>`
+    template: `
+        <mat-card>
+            <mat-card-content>
+                <h3>Settings</h3>
+
+                    <mat-form-field>
+                        <mat-select placeholder="Select application theme" [(ngModel)]="selectedValue" name="food">
+                            <mat-option *ngFor="let food of foods" [value]="food.value">
+                                {{food.viewValue}}
+                            </mat-option>
+                        </mat-select>
+                    </mat-form-field>
+
+                    <p> Selected value: {{selectedValue}} </p>
+
+                    <mat-checkbox color="primary"
+                                  (change)="showOptions()"
+                                  [(ngModel)]="showPhoto">
+                        Show user Photo
+                    </mat-checkbox>
+            </mat-card-content>
+        </mat-card>
+    `
 })
-export class SettingsComponent { }
+export class SettingsComponent {
+    selectedValue: string;
+
+    showPhoto = true;
+
+    foods = [
+        {value: 'theme-1', viewValue: 'Theme 1'},
+        {value: 'theme-2', viewValue: 'Theme 2'},
+        {value: 'theme-3', viewValue: 'Theme 3'}
+    ];
+
+    showOptions(): void {
+        console.log(this.showPhoto);
+    }
+}
