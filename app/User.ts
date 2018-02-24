6122dd2cf97e27ac206d2ca0b616440acd3eefcd
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-02-24 18:18:39 +0300

    add user details markup, add avatar logic

--- app/User.ts
+++ app/User.ts
@@ -5,13 +5,15 @@ export class User {
     phone: number;
     date: string;
     position: string;
+    avatar: string;
 
-    constructor(id: number, name: string, email: string, phone: number, date: string, position: string) {
+    constructor(id: number, name: string, email: string, phone: number, date: string, position: string, avatar?: string) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.date = date;
         this.position = position;
+        this.avatar = avatar || '';
     }
 }
--- app/components/UsersCmp/app.user-details.ts
+++ app/components/UsersCmp/app.user-details.ts
@@ -9,20 +9,65 @@ import { UserService } from '../../user.service';
 @Component({
     selector: 'app-user-details',
     template: `
-        <div *ngIf="item">
-            <span>User Details {{ id }}</span>
-            <span>User Name {{ item.name }}</span>
-        </div>
+        <mat-card *ngIf="item">
+            <h3>User details</h3>
+
+            <mat-card-content>
+                <div class="avatar-block">
+                    <div class="avatar">
+                        <img class="avatar-img" [src]="getAvatar()" alt="user-avatar">
+                    </div>
+                </div>
+                <p><span class="bold">User name:&nbsp;</span>{{item.name}}</p>
+                <p><span class="bold">User email:&nbsp;</span>{{item.email}}</p>
+                <p><span class="bold">User telephone:&nbsp;</span>{{item.phone}}</p>
+                <p><span class="bold">User hiring date:&nbsp;</span>{{item.date}}</p>
+                <p><span class="bold">User position:&nbsp;</span>{{item.position}}</p>
+            </mat-card-content>
+
+            <mat-card-actions>
+                <a class="btn-link" mat-button routerLink="/users">BACK</a>
+            </mat-card-actions>
+        </mat-card>
     `,
+    styles: [`
+        .avatar-block {
+            padding: 10px 0;
+        }
+
+        .avatar {
+            width: 100px;
+            height: 100px;
+            overflow: hidden;
+            border-radius: 50%;
+        }
+
+        .avatar-img {
+            width: 100%;
+        }
+
+        .bold {
+            display: inline-block;
+            min-width: 200px;
+            font-size: 1.1em;
+            font-weight: bold;
+        }
+
+        .btn-link {
+            text-decoration: none;
+        }
+    `],
     providers: [UserService]
 })
 export class UserDetailsComponent implements OnInit {
     item: User;
     error: any;
     id: number;
+    dafaultAvatar: string;
 
     constructor(private userService: UserService, private activateRoute: ActivatedRoute) {
         this.id = activateRoute.snapshot.params['id'];
+        this.dafaultAvatar = 'assets/img/avatar.jpeg';
     }
 
     ngOnInit() {
@@ -34,5 +79,9 @@ export class UserDetailsComponent implements OnInit {
             .getUserInfo(+this.id)
             .then(data => this.item = data)
             .catch(error => this.error = error);
+    }
+
+    getAvatar() {
+        return this.item.avatar ? this.item.avatar : this.dafaultAvatar;
     }
 }
